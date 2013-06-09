483e10cf5849089d4bcfa94d5044a5b7b82c5a8e
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-06-09 22:37:59 +0900

    First Commit
    Xcodeプロジェクト作成

---
+++ ProgPhotoTreatment/ProgPhotoTreatment/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ProgPhotoTreatment
+//
+//  Created by kazuyoshi kawakami on 13/06/09.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ProgPhotoTreatment/ProgPhotoTreatment/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProgPhotoTreatment
+//
+//  Created by kazuyoshi kawakami on 13/06/09.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
